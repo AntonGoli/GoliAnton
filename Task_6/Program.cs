6b0edb7c116545a5078d14d4dba7161af5afc4d7
Goli Anton <fongoli@yandex.ru>
2022-09-05 17:28:32 +0300

    Задание_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+void Max (int arg1) 
+{
+    if (arg1%2 == 0) {
+        Console.WriteLine ($"{arg1} - чётное число");
+    }
+    else {
+        Console.WriteLine ($"{arg1} - нечётное число");
+    } 
+}
+
+Console.WriteLine("Введите число.");
+int a = int.Parse(Console.ReadLine());
+
+Max(a);
