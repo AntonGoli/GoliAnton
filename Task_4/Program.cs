ab26086551cc9ebdd8924df2db14ace5e5b3e467
Goli Anton <fongoli@yandex.ru>
2022-09-05 15:13:27 +0300

    Задание2

---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int UsInp (int l) 
+{
+    Console.WriteLine("Введите число.");
+    l = int.Parse(Console.ReadLine()); 
+    return l;
+}
+
+int FindMax (int arg1, int arg2, int arg3) 
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a = 0; int b = 0; int c = 0;
+a = UsInp(a); b = UsInp(b); c = UsInp(c);
+
+Console.WriteLine($"Наибольшее из данных чисел - {FindMax(a,b,c)}.");
